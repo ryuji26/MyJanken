a99b0f835dd27d0fc7c0fa71c99347ad53569d7c
ryuji <ryujispjp@gmail.com>
2021-09-27 16:22:12 +0900

    Initial Commit

---
+++ MyJanken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyJanken
+//
+//  Created by 髙橋　竜治 on 2021/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
